9b409af65d307406496b5a5bc96672738e1b18cb
donorp <donorp@arhat.solutions>
2021-07-14 11:50:57 +0200

    chore(proton-bridge): Update image tag
    Signed-off-by: donorp <donorp@arhat.solutions>

--- scripts/helm.sh
+++ scripts/helm.sh
@@ -18,7 +18,10 @@ gen() {
   target="$1"
 
   mkdir -p build
-  helm template "${target}" "./charts/${target}" --include-crds --namespace "${NAMESPACE}" --debug > "build/${target}.yaml"
+  helm template "${target}" "./charts/${target}" \
+    --include-crds \
+    --namespace "${NAMESPACE}" \
+    --debug > "build/${target}.yaml"
 }
 
 CMD=$(printf "%s" "$@" | tr '.' ' ')
